e49db27e7c0d9b44645e9a773763a83e07de314c
4c <4c@INF16>
2022-04-27 12:33:50 +0200

    Hakuna Matata Ragazzi

--- src/MainClass.java
+++ src/MainClass.java
@@ -10,6 +10,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Ciurlia Rizzello");
 		System.out.println("Ponzetta Fabiano");
+		System.out.println("Turco Simone");
 
 	}
 
